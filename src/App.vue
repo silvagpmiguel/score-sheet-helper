edbe3a2b44b8b4fcb464a21fdeb9b84fc562de17
miguel.silva <miguel.silva@vilt-group.com>
2020-07-04 23:23:17 +0100

    csv generator

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <MainPage id="app"/>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MainPage from "./components/MainPage.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    MainPage
   }
 };
 </script>
@@ -23,6 +20,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 2rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from "vue"
+import App from "./App.vue"
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.min.css'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-Vue.config.productionTip = false;
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
-}).$mount("#app");
+}).$mount("#app")
